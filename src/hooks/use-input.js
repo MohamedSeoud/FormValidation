bd283c6f55e0a0ae22293290c8b01141ac2e3d41
Mohammed Seoud <m.seoud42@gmail.com>
2023-06-24 13:08:43 +0300

    FinalCommit

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -10,7 +10,7 @@ const inputStateReducer = (state, action) => {
     return { value: action.value, isTouched: state.isTouched };
   }
   if (action.type === 'BLUR') {
-    return { isTouched: true};
+    return { isTouched: true, value: state.value };
   }
   if (action.type === 'RESET') {
     return { isTouched: false, value: '' };
